cf3aa035aff805b25750d73bcf2fd96ce742e1fb
Dani <Dani4kor@gmail.com>
2016-09-22 00:20:14 +0300

    fix PEP8

--- oauthselenium.py
+++ oauthselenium.py
@@ -4,10 +4,9 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
-
 APPID = '5637720'
-LOGIN = '' 
-PASSWORD = '' 
+LOGIN = ''
+PASSWORD = ''
 
 driver = webdriver.Chrome()
 driver.get("http://api.vkontakte.ru/oauth/authorize?"
@@ -24,18 +23,18 @@ submit = driver.find_element_by_id("install_allow")
 submit.click()
 
 try:
-	driver.implicitly_wait(2) 
-	submit = driver.find_element_by_id("install_allow")
-	submit.click()
+    driver.implicitly_wait(2)
+    submit = driver.find_element_by_id("install_allow")
+    submit.click()
 except NoSuchElementException:
-	print "TOKEN has already been received"
-finally:	
-	urllist = (driver.current_url.split("#"))[1].split("&")
-	access_token, expires_in, user_id = (urllist[0].split("="))[1], \
-										(urllist[1].split("="))[1], \
-										(urllist[2].split("="))[1]
-	
-	print driver.current_url
-	print user_id, access_token, expires_in
+    print "TOKEN has already been received"
+finally:
+    urllist = (driver.current_url.split("#"))[1].split("&")
+    access_token, expires_in, user_id = (urllist[0].split("="))[1], \
+                                        (urllist[1].split("="))[1], \
+                                        (urllist[2].split("="))[1]
 
-	driver.close()
+    print driver.current_url
+    print user_id, access_token, expires_in
+
+    driver.close()
